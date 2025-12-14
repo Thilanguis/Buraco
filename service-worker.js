d110dd9346fe93b934a689000fc290f98eb4fca7
Gabriel Granado <46542929+Thilanguis@users.noreply.github.com>
2025-12-13 20:16:52 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'buraco-v10';
+const CACHE_NAME = 'buraco-v11';
 
 // Coloca aqui os arquivos estáticos que você quer cachear
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
