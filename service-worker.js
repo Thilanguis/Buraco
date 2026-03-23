78d6b25178381689c14c40d7ffb1f06e426c2c9c
Thilanguis <gabrielt2005@hotmail.com>
2026-03-22 23:33:40 -0400

    erro usuário batendo sem limpa com carta do lixo

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'buraco-v43';
+const CACHE_NAME = 'buraco-v44';
 
 // Removido o "/icons/" do caminho das imagens
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
