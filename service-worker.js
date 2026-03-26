d9203671f658d1cf6e78f9048bc56ef6989badfe
Thilanguis <gabrielt2005@hotmail.com>
2026-03-25 21:45:52 -0400

    fix: Corrige falso-negativo na validacao de jogos com o 2 natural
    - fix(engine): Implementa simulador de estado (simulateMeld) ao estender jogos na mesa
    - fix(engine): Remove temporariamente as travas visuais (forceNatural/forceWild) do clone da mesa para permitir que a matematica reavalie o 2 como coringa
    - fix(ux): Permite que jogadores adicionem cartas a um jogo onde o 2 estava atuando como carta natural sem tomar erro de sequencia invalida

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'buraco-v47';
+const CACHE_NAME = 'buraco-v48';
 
 // Removido o "/icons/" do caminho das imagens
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
