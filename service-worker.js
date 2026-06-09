909f4d3069533853a8152e425bad76eb5f92e373
Thilanguis <gabrielt2005@hotmail.com>
2026-06-09 16:09:32 -0400

    Adversário ve cronômetro tb se o jogador ativo receber atualização durante jogada.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'buraco-v77'; // Força atualização
+const CACHE_NAME = 'buraco-v78'; // Força atualização
 
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
